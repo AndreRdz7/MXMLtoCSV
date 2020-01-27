e4d370e57ebf101562386b9d6a56516d16d47bf3
André Rodríguez <andrerome@geekyducky.com>
2020-01-26 22:18:53 -0600

    correction for mis-git-push on repo, fixed repo

--- src/main/java/mx/tec/mining/Main.java
+++ src/main/java/mx/tec/mining/Main.java
@@ -1,40 +1,32 @@
 package mx.tec.mining;
 
 import org.deckfour.xes.model.*;
-import org.deckfour.xes.model.impl.XLogImpl;
-import org.deckfour.xes.out.XSerializer;
-import org.deckfour.xes.out.XesXmlSerializer;
-
-import java.io.FileOutputStream;
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.Map;
+import java.io.PrintStream;
 
 public class Main {
     public static void main(String[] args) throws Exception {
 
         XLog log = XLogReader.openLog("simulation_logs.mxml.gz");
+        PrintStream out = new PrintStream("log.csv");
+        out.println("CaseID,Activity,EventType,Resource,Timestamp");
+
         for (XTrace trace: log) {
-            Iterator<XEvent> it = trace.iterator();
-            Map<String, XAttribute> map = new HashMap<>();
-            while (it.hasNext()) {
-                XEvent event = it.next();
-                XAttributeMap attributes = event.getAttributes();
-                String label = attributes.get("concept:name").toString();
-                if(attributes.containsKey("org:resource")) {
-                    map.put(label, attributes.get("org:resource"));
-                }
-                if (map.containsKey(label)) {
-                    attributes.put("org:resource", map.get(label));
-                }
-                else{
-                    it.remove();
+            String caseId = trace.getAttributes().get("concept:name").toString();
+            for (XEvent event: trace) {
+                XAttributeMap attrs = event.getAttributes();
+                try {
+                    String str = caseId + ","
+                            + attrs.get("concept:name").toString() + ","
+                            + attrs.get("lifecycle:transition").toString() + ","
+                            + attrs.get("org:resource").toString() + ","
+                            + attrs.get("time:timestamp").toString();
+                    out.println(str);
+                } catch ( NullPointerException e ) {
+                    e.printStackTrace();
                 }
             }
         }
-        XSerializer serializer = new XesXmlSerializer();
-        serializer.serialize(log, new FileOutputStream("simulation_logs.xes"));
-        // Add namespace xmlns="http://www.xes-standard.org" if there is no namespace
+        out.close();
         System.out.println("Process Finished");
     }
 }
